a66e80e3624586e160c83fdd6edae5bce72db77d
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-08 23:43:53 +0600

    Create a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Header } from "./components/Header"
 import { Navbar } from "./components/Navbar"
 
 function App() {
@@ -7,39 +8,8 @@ function App() {
             
 
             <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-                {/* header */}
-                <div>
-                    <form
-                        class="flex items-center bg-gray-100 px-4 py-4 rounded-md"
-                    >
-                        <img
-                            src="./images/notes.png"
-                            class="w-6 h-6"
-                            alt="Add todo"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Type your todo"
-                            class="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
-                        />
-                        <button
-                            type="submit"
-                            class="appearance-none w-8 h-8 bg-[url('./images/plus.png')] bg-no-repeat bg-contain"
-                        ></button>
-                    </form>
-
-                    <ul class="flex justify-between my-4 text-xs text-gray-500">
-                        <li class="flex space-x-1 cursor-pointer">
-                            <img
-                                class="w-4 h-4"
-                                src="./images/double-tick.png"
-                                alt="Complete"
-                            />
-                            <span>Complete All Tasks</span>
-                        </li>
-                        <li class="cursor-pointer">Clear completed</li>
-                    </ul>
-                </div>
+                <Header />
+                
                 <hr class="mt-4" />
 
                 {/* todo list */}
